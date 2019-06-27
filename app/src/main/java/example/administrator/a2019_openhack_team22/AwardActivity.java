7fbe8f739fa15ccf672e354788c561cf34e75a31
PhireRed2015 <davkim1030@gmail.com>
2019-06-27 13:05:04 +0900

    AwardActivity Lottie, layout 추가 json 파일들 추가

---
+++ app/src/main/java/example/administrator/a2019_openhack_team22/AwardActivity.java
@@ -0,0 +1,13 @@
+package example.administrator.a2019_openhack_team22;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AwardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_award);
+    }
+}
