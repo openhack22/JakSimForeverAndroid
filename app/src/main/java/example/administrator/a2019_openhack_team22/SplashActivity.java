82154da8875343a943d03bb379fc5063054185e8
PhireRed2015 <davkim1030@gmail.com>
2019-06-28 01:42:47 +0900

    setContentView 추가

--- app/src/main/java/example/administrator/a2019_openhack_team22/SplashActivity.java
+++ app/src/main/java/example/administrator/a2019_openhack_team22/SplashActivity.java
@@ -1,28 +1,28 @@
 package example.administrator.a2019_openhack_team22;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
-public class SplashActivity extends Activity {
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-            ImageView imageView = (ImageView) findViewById(R.id.splash);
-            GlideDrawableImageViewTarget gifImage = new GlideDrawableImageViewTarget(imageView);
-            Glide.with(this).load(R.drawable.splash).into(gifImage);
+        ImageView imageView = findViewById(R.id.splash);
+        GlideDrawableImageViewTarget gifImage = new GlideDrawableImageViewTarget(imageView);
+        Glide.with(this).load(R.drawable.splash).into(gifImage);
 
-            Handler hd = new Handler();
-            hd.postDelayed(new splashhandler(), 3000); // 1초 후에 hd handler 실행  3000ms = 3초
+        Handler hd = new Handler();
+        hd.postDelayed(new splashhandler(), 3000); // 1초 후에 hd handler 실행  3000ms = 3초
 
-        }
+    }
 
     private class splashhandler implements Runnable{
         public void run(){
