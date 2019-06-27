935d8fb05a3878622728cf1b16a442e4691701ce
PhireRed2015 <davkim1030@gmail.com>
2019-06-27 18:59:47 +0900

    AwardActivity에 MoneyCounter 애니메이션 추가, 디자인 변경

--- app/src/main/java/example/administrator/a2019_openhack_team22/AwardActivity.java
+++ app/src/main/java/example/administrator/a2019_openhack_team22/AwardActivity.java
@@ -1,10 +1,12 @@
 package example.administrator.a2019_openhack_team22;
 
 import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
@@ -20,6 +22,8 @@ public class AwardActivity extends AppCompatActivity {
     ConstraintLayout layoutCost;    // 돈 결과 나오는 레이아웃
     TextView txtWinnerUserName; // 우승한 유저 이름
     TextView txtAmount;     // setText 할 때 "원"붙여줘야함
+    ImageView imgCircle;
+    int amount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,20 +36,21 @@ public class AwardActivity extends AppCompatActivity {
         txtWinnerUserName = findViewById(R.id.txt_winner_name);
         txtAmount = findViewById(R.id.txt_cost);
         layoutCost = findViewById(R.id.layout_cost);
+        imgCircle = findViewById(R.id.img_circle);
 
         if(isWin){
             lottieAnimationView.setAnimation("677-trophy.json");
             txtResult.setText("축하합니다!");
             txtRank.setText("우승");
             // TODO: 유저네임 받아야함
-            txtAmount.setText(NumberFormat.getInstance().format(19000) + "원");
+            amount = 190000;
         }
         else{
             lottieAnimationView.setAnimation("6627-dislike-thumb-emoji.json");
             txtResult.setText("다음엔 돈을\n조금만 걸어보세요...!");
             txtRank.setText("실패");
             // TODO: 유저네임 받아야함
-            txtAmount.setText(NumberFormat.getInstance().format(-19000) + "원");
+            amount = -190000;
         }
         // lottie 애니메이션 실행
         lottieAnimationView.playAnimation();
@@ -66,6 +71,8 @@ public class AwardActivity extends AppCompatActivity {
                 txtWinnerUserName.setVisibility(View.VISIBLE);
                 txtResult.setVisibility(View.VISIBLE);
                 layoutCost.setVisibility(View.VISIBLE);
+                startCountAnimation(txtAmount, amount);
+                imgCircle.setVisibility(View.VISIBLE);
             }
 
             @Override
@@ -77,5 +84,16 @@ public class AwardActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void startCountAnimation(final TextView number, int amount) {
+        ValueAnimator animator = ValueAnimator.ofInt(0, amount); //0 is min number, 600 is max number
+        animator.setDuration(1000); //Duration is in milliseconds
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            public void onAnimationUpdate(ValueAnimator animation) {
+                number.setText(NumberFormat.getInstance().format(Integer.valueOf(animation.getAnimatedValue().toString())) + "원");
+            }
+        });
+        animator.start();
     }
 }
